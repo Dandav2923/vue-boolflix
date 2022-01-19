e00181f8ddabdb34b5b363d8297fe20064a23927
Dandav2923 <daniele.emili23@gmail.com>
2022-01-19 13:46:29 +0100

    placeholder input

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+      <div class="row p-3">
+          <div class="col-4">
+              <div class="input-group mb-3">
+                <button 
+                    class="btn btn-outline-secondary" 
+                    type="button" 
+                    id="button-addon1">Button</button>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    placeholder="Inserisci il titolo del film" 
+                    aria-label="Example text with button addon" aria-describedby="button-addon1">
+            </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
