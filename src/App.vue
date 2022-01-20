066e9748eaa82b749e3b70bc36b22752152f6bf3
Dandav2923 <daniele.emili23@gmail.com>
2022-01-20 18:21:41 +0100

    spazi

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,55 @@
 // * eslint-disable *
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+<div id="app">
     <Header @searchFilms="searchFilms($event)"/>
     <Main  
         :films="films"
         :series="series"
-     />
-  </div>
+    />
+</div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 import axios from 'axios';
 
 export default {
-  name: "App",
-  components: {
-    // HelloWorld,
+name: "App",
+components: {
     Header,
     Main,
-  },
-  data() {
-        return {
-            inputTextSearch:'',
-            films: null,
-            series: null,
-            query: 'https://api.themoviedb.org/3/search/movie',
-            querySeries: 'https://api.themoviedb.org/3/search/tv'
-            
-        }
+},
+data() {
+    return {
+        inputTextSearch:'',
+        films: null,
+        series: null,
+        query: 'https://api.themoviedb.org/3/search/movie',
+        querySeries: 'https://api.themoviedb.org/3/search/tv'
+    }
+},
+methods: {
+    searchFilms(value) {
+        this.inputTextSearch = value
+        console.log(this.inputTextSearch);
+        this.callAxiosFilm();
     },
-    methods: {
-        searchFilms(value) {
-            this.inputTextSearch = value
-            console.log(this.inputTextSearch);
-            this.callAxiosFilm();
-        },
-        callAxiosFilm () {
-            axios.get(this.query, {
+    callAxiosFilm () {
+        axios.get(
+            this.query, {
                 params: {
                     api_key: 'df5b914f4cb568985555883d8eeec9a2',
                     query: this.inputTextSearch
                 }
-            })
-            .then((results) => {
-                this.films = results.data.results;
-                console.log(this.films);
+            }
+        )
+        .then((results) => {
+            this.films = results.data.results;
+            console.log(this.films);
             });
-            axios.get(this.querySeries, {
+        axios.get(
+            this.querySeries, {
                 params: {
                     api_key: 'df5b914f4cb568985555883d8eeec9a2',
                     query: this.inputTextSearch
